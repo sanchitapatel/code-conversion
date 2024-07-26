6985515db1f81c0fc55af11a7976dd11b12e208c
sanchitapatel <sanchita15pa@gmail.com>
2024-07-25 18:36:46 -0700

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+import json 
+python_da={
+    'Name':'sanchita',
+    'qualification':'m.tech',
+    'belongs_to_single':'False',
+    'activate':'None'
+
+}
+print(json.dumps(python_da))
+#python to json code convert
